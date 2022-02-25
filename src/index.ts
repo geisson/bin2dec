0630929736b6080efc3f84eda5e3b07c86ae4471
Geisson Iuri <geissoniuri@gmail.com>
2022-02-25 07:05:18 -0300

    app funcionando

--- src/index.ts
+++ src/index.ts
@@ -7,22 +7,20 @@ const containerErrorBinary = document.querySelector('[data-erro=error-binary') a
 function handleKeypress(this: HTMLInputElement, e: Event): void {
   const binaryInputValue = this.value;
 
-  const lastDigit = +binaryInputValue.substr(-1);
-  if (lastDigit !== 0 && lastDigit !== 1) {
-    containerErrorBinary.classList.remove('invisible');
-    binaryNumberInput.classList.add('input-error');
-  } else {
-    containerErrorBinary.classList.add('invisible');
-    binaryNumberInput.classList.remove('input-error');
-  }
-
-  console.log(`ultimo digitado: ${lastDigit}`);
+  const ExpCheckDifferentNumber = /([^01])/;
+  const verifiedValue = new RegExp(ExpCheckDifferentNumber).exec(binaryInputValue);
 
   const convertBinaryToDecimal = parseInt(binaryInputValue, 2);
 
-  console.log(!convertBinaryToDecimal);
-
-  decimalNumberInput.value = convertBinaryToDecimal.toString();
+  if (verifiedValue) {
+    containerErrorBinary.classList.remove('invisible');
+    binaryNumberInput.classList.add('input-error');
+    decimalNumberInput.value = 'binary number invalid';
+  } else {
+    containerErrorBinary.classList.add('invisible');
+    binaryNumberInput.classList.remove('input-error');
+    decimalNumberInput.value = convertBinaryToDecimal.toString();
+  }
 }
 
-binaryNumberInput?.addEventListener('input', handleKeypress);
+binaryNumberInput.addEventListener('input', handleKeypress);
